e7f7188849d2fa91b824978f622abe515e7b873b
allucassilva-2025 <al.lucas.silva@impatech.edu.br>
2026-06-06 00:18:56 -0300

    Organize author images and graph data

---
+++ data/graph-data.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const authorNames = Array.from(
+  { length: 25 },
+  (_, index) => "Autor " + (index + 1)
+);
+
+const subjectNames = Array.from(
+  { length: 60 },
+  (_, index) => "Matéria " + (index + 1)
+);
